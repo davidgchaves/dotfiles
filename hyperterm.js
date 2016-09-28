6d55c56e0f39fdac0a49cb287f26a92460fcefb2
David G Chaves <davidgchaves@gmail.com>
2016-09-28 22:18:49 +0200

    hyperterm: add FiraCode-Retina font

--- hyperterm.js
+++ hyperterm.js
@@ -1,10 +1,10 @@
 module.exports = {
   config: {
     // default font size in pixels for all tabs
-    fontSize: 12,
+    fontSize: 14,
 
     // font family with optional fallbacks
-    fontFamily: 'Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: '"FiraCode-Retina", Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: 'rgba(248,28,229,0.75)',
